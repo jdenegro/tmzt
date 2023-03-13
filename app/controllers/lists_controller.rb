569e108537be9234ca54b74c9747a293f02ac813
Alberto de Negro <juniordn825@gmail.com>
2023-03-13 11:55:46 +0100

    api implementation done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,29 @@
+require 'json'
+require 'open-uri'
+
 class ListsController < ApplicationController
   before_action :set_list, only: :show
   skip_before_action :authenticate_user!, only: :index
+  # TODO: validate @Lists to show on view
 
   def index
-    @movies = Movie.all
-    @lists = List.all
+    # @movies = movie.all
+    # raise
+    # @lists = "aaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+    # TODO: Save URL with API Key in variable
+    # TODO: Use Open URI to access data from URL, use movie_serialized
+    # TODO: Parse data into JSON.
+    url = "https://api.themoviedb.org/3/discover/movie?api_key=2d904f2ccbadaa1303f4b2e7ea571a93&"
+    @movies = JSON.parse(URI.open(url).read)["results"]
+    # movie_serialized = URI.open(url).read
+    # movie = JSON.parse(movie_serialized)
+    # movie["results"]
+    # * Use the tutorial from Kitt
+    # * Use raise to check data response ALL THE TIME
+    # * If stuck make ticket!!!! :)
+
+
   end
 
   def show
